e61a1a571eae422556970928ceeed5a603060f93
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-07-25 14:01:00 -0600

    adds honeydew service functions

---
+++ src/services/honeydewService.js
@@ -0,0 +1,54 @@
+import * as tokenService from './tokenService'
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/honeydew`
+
+async function create(honeydew) {
+  const res = await fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`, // Form Data notification for the back-end
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(honeydew)
+  })
+  return res.json()
+}
+
+async function getAll() {
+  const res = await fetch(BASE_URL)
+  return res.json()
+}
+
+async function getOne(id) {
+  const res = await fetch(`${BASE_URL}/${id}`)
+  return res.json()
+}
+
+async function deleteOne(id) {
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    }
+  })
+  return res.json()
+}
+
+async function update(drink) {
+  const res = await fetch(`${BASE_URL}/${drink._id}`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(drink)
+  })
+  return res.json()
+}
+
+export {
+  create,
+  getAll,
+  getOne,
+  deleteOne,
+  update,
+}

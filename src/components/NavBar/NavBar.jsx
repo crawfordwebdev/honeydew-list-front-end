a85257349c7917535092eef43bdb6d983eabf8ec
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-07-26 21:06:09 -0600

    updates navbar items

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,26 +1,40 @@
 import { Link } from 'react-router-dom'
+import styles from './NavBar.module.css'
+import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from '@mui/icons-material/Login';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import KeyIcon from '@mui/icons-material/Key';
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <>
+    <div className={styles.navbar}>
       {user ?
         <nav>
           <ul>
-            <li><Link to="/honeydews">Honeydews List</Link></li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
+            <li><Link to="/honeydews">My Honeydews</Link></li>
+            <span className={styles.buttons}>
+            <li>
+              <Link to="/changePassword">
+                <KeyIcon sx={{transform: 'scale(1.5)'}}/>
+              </Link>
+            </li>
+            <li>
+              <Link to="" onClick={handleLogout}>
+                <LogoutIcon sx={{transform: 'scale(1.5)'}}/>
+              </Link>
+            </li>
+            </span>
           </ul>
         </nav>
       :
         <nav>
           <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/login">Login <LoginIcon /></Link></li>
+            <li><Link to="/signup">Register <AppRegistrationIcon /></Link></li>
           </ul>
         </nav>
       }
-    </>
+    </div>
   )
 }
 
